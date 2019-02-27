b961703940bcefacc0333345665579a7bc4f6b63
Raul <raulsoriano848@gmail.com>
2019-02-27 16:53:24 +0100

    Formulario con validación en Spring
    Aplicación con formulario, envio de datos a controlador y validación en Spring.

--- SpringForm/src/main/java/com/example/demo/Controlador.java
+++ SpringForm/src/main/java/com/example/demo/Controlador.java
@@ -1,6 +1,10 @@
 package com.example.demo;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,7 +26,12 @@ public class Controlador implements WebMvcConfigurer{
 	}
 	
 	@PostMapping("/form")
-	public String getResult() {
-			return "result";	
+	public String getResult(@Valid Person person, BindingResult bindingResult,Model model) {
+		if(bindingResult.hasErrors()) {
+			
+			return "form";
+		}	
+		model.addAttribute("Persona", person);
+		return "result";	
 	}
 }
--- SpringForm/src/main/java/com/example/pojo/Person.java
+++ SpringForm/src/main/java/com/example/pojo/Person.java
@@ -1,10 +1,18 @@
 package com.example.pojo;
 
+import javax.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Range;
+
+
 public class Person {
 
+	@NotEmpty
 	private String nombre;
+	
 	private String apellidos;
+	@Range(min = 9, max = 9)
 	private int telefono;
+	
 	private String correo;
 	
 	public String getNombre() {
@@ -25,7 +33,12 @@ public class Person {
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
-	
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
 	@Override
 	public String toString() {
 		return "Person [nombre=" + nombre + ", apellidos=" + apellidos + ", telefono=" + telefono + ", correo=" + correo
